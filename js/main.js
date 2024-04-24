a8721b4f318e3c8805d6c6abaa05e82bd8ffc04a
Cal Doval <cydoval@mac.com>
2024-04-23 21:03:03 -0600

    Revert "final filter"
    This reverts commit 222d84c7621f0dc5e4b74fd02d7b2992142831bc.

--- js/main.js
+++ js/main.js
@@ -2,195 +2,142 @@
 MyApp.spreadsheetData = [];
 MyApp.keywords = [];
 MyApp.headerData = [
-  { sTitle: "University" },
-  { sTitle: "State" },
-  { sTitle: "Public/Private" },
-  { sTitle: "Type" },
-  { sTitle: "Name" },
-  { sTitle: "Focus Areas" },
-  { sTitle: "Center focus details" },
+    { "sTitle": "Title" }, { "sTitle": "Authors" }, { "sTitle": "Source" }, { "sTitle": "Year" }, { "sTitle": "keywords" }
 ];
-// filter options start here
-MyApp.filterIndexes = { state: 1, type: 3 };
-MyApp.States = [];
-MyApp.Types = [];
-MyApp.Publicprivates = [];
 
 String.prototype.trunc = function (n) {
-  return this.substr(0, n - 1) + (this.length > n ? "&hellip;" : "");
+    return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
 };
 
 $(function () {
-  var url =
-    "https://sheets.googleapis.com/v4/spreadsheets/1PxbyURdyAW2588lBbh75v0xlx0zB7mEilzgJqH0Kp9s/values/Database_directory?key=AIzaSyA3dk7j-VOX78HlLFqsOEHNL5rDljrMtIA";
-  $.getJSON(url, {}, function (data) {
-    for (let i = 1; i < data["values"].length; i++) {
-      const currRow = data["values"][i];
-      var university = currRow[0];
-      var state = currRow[1];
-      var publicprivate = currRow[2];
-      var type = currRow[3];
-      var link = currRow[4];
-      var focusareas = currRow[5];
-      var centerfocusareas = currRow[6] || "";
-      var keyword = currRow[7] || "";
+    var url = "https://sheets.googleapis.com/v4/spreadsheets/1rKvO6WdHDj-J4XfAf3Ms_sKgVZnFb9VaAfx-gHijZKI/values/Individual_Research?key=AIzaSyA3dk7j-VOX78HlLFqsOEHNL5rDljrMtIA";
+    $.getJSON(url, {}, function (data) {
+        for (let i = 1; i < data["values"].length; i++) {
+            const currRow = data["values"][i];
 
-      MyApp.spreadsheetData.push([
-        GenerateTitleColumn(university, link),
-        state,
-        publicprivate,
-        type,
-        link,
-        focusareas,
-        centerfocusareas,
-        keyword,
-      ]);
-      // filters step two
-      if ($.inArray(state, MyApp.States) === -1 && state.length !== 0) {
-        MyApp.States.push(state);
-      }
-      if ($.inArray(type, MyApp.Types) === -1 && type.length !== 0) {
-        MyApp.Types.push(type);
-      }
-      if (
-        $.inArray(publicprivate, MyApp.Publicprivates) === -1 &&
-        publicprivate.length !== 0
-      ) {
-        MyApp.Publicprivates.push(publicprivate);
-      }
-    }
+            var title = currRow[0];
+            var authors = currRow[1];
+            var source = currRow[2];
+            var year = currRow[3];
+            var keyword = currRow[7] || "";
+            var abstract = currRow[6];
+            var link = currRow[5];
 
-    createDataTable();
-    addFilters();
-  });
-});
+            MyApp.spreadsheetData.push(
+                [
+                    GenerateTitleColumn(title, abstract, link), authors, source, year, keyword
+                ]);
 
-function GenerateTitleColumn(university, link) {
-  //entry value from spreadsheet
-  return (
-    "<a href='" +
-    link +
-    "' class='abstract-popover' data-toggle='popover'>" +
-    university +
-    "</a>"
-  );
-}
-
-function addFilters() {
-  // filters step three
-  var $statesfilter = $("#state");
-
-  $.each(MyApp.States, function (key, val) {
-    $statesfilter.append(
-      '<li><label><input type="checkbox" name="' +
-        val +
-        '"> ' +
-        val +
-        "</label></li>"
-    );
-  });
-  var $typesfilter = $("#type");
-
-  $.each(MyApp.Types, function (key, val) {
-    $typesfilter.append(
-      '<li><label><input type="checkbox" name="' +
-        val +
-        '"> ' +
-        val +
-        "</label></li>"
-    );
-  });
-  var $publicprivatefilter = $("#publicprivate");
-
-  $.each(MyApp.Publicprivates, function (key, val) {
-    $publicprivatefilter.append(
-      '<li><label><input type="checkbox" name="' +
-        val +
-        '"> ' +
-        val +
-        "</label></li>"
-    );
-  });
-
-  $(".filterrow").on("click", "ul.filterlist", function (e) {
-    var filterRegex = "";
-    var filterName = this.id;
-    var filterIndex = MyApp.filterIndexes[filterName];
-
-    var filters = [];
-    $("input", this).each(function (key, val) {
-      if (val.checked) {
-        if (filterRegex.length !== 0) {
-          filterRegex += "|";
+            if ($.inArray(keyword, MyApp.keywords) === -1 && keyword.length !== 0) {
+                MyApp.keywords.push(keyword);
+            }
         }
 
-        filterRegex += val.name; //Use the hat and dollar to require an exact match
-      }
+        MyApp.keywords.sort();
+
+        createDataTable();
+        addFilters();
+        abstractPopup();
+    });
+})
+
+function GenerateTitleColumn(title, abstract, link) { //entry value from spreadsheet
+    return "<a href='" + link + "' class='abstract-popover' data-toggle='popover' data-content='" + abstract + "' data-original-title='Abstract'>" + title + "</a>";
+}
+
+function abstractPopup() {
+    $("#spreadsheet").popover({
+        selector: '.abstract-popover',
+        trigger: 'hover'
+    });
+}
+
+function addFilters(){
+    var $filter = $("#filter_elements");
+    
+    $.each(MyApp.keywords, function (key, val) {
+        $filter.append('<li><label><input type="checkbox" name="' + val + '"> ' + val + '</label></li>');
+    });
+        
+    $filter.on("change", function (e) {
+        e.preventDefault();
+        var selected = this.name;
+
+        var filterRegex = "";
+        var filters = [];
+
+        $("input:checkbox", this).each(function (key, val) {
+            if (val.checked) {
+                if (filterRegex.length !== 0) {
+                    filterRegex += "|";
+                }
+
+                filterRegex += "(^" + val.name + "$)"; //Use the hat and dollar to require an exact match
+            }
+        });
+
+        console.log(filterRegex);
+        MyApp.oTable.fnFilter(filterRegex, 4, true, false);
+        displayCurrentFilters();
     });
 
-    console.log(filterRegex);
-    MyApp.oTable.fnFilter(filterRegex, filterIndex, true, false);
-    displayCurrentFilters();
-  });
+    $("#clearfilters").click(function (e) {
+        e.preventDefault();
 
-  $("#clearfilters").click(function (e) {
-    e.preventDefault();
+        $(":checkbox", $filter).each(function () {
+            this.checked = false;
+        });
 
-    $(":checkbox", $filter).each(function () {
-      this.checked = false;
+        $filter.change();
     });
-
-    $filter.change();
-  });
 }
 
 function displayCurrentFilters() {
-  var $filterAlert = $("#filters");
+    var $filterAlert = $("#filters");
+    
+    var filters = "";
+    
+    $(":checked", "#filter_elements").each(function () {
+        if (filters.length !== 0) {
+            filters += " + "
+        }
+        filters += "<strong>" + this.name + "</strong>";
+    });
 
-  var filters = "";
-
-  $(":checked", "#filter_elements").each(function () {
     if (filters.length !== 0) {
-      filters += " + ";
+        var alert = $("<div class='alert alert-info'><strong>Filters</strong><p>You are filtering on " + filters + "</p></div>")
+
+        $filterAlert.html(alert);
+        $filterAlert[0].scrollIntoView(true);
+    } else {
+        $filterAlert.html(null);
     }
-    filters += "<strong>" + this.name + "</strong>";
-  });
-
-  if (filters.length !== 0) {
-    var alert = $(
-      "<div class='alert alert-info'><strong>Filters</strong><p>You are filtering on " +
-        filters +
-        "</p></div>"
-    );
-
-    $filterAlert.html(alert);
-    $filterAlert[0].scrollIntoView(true);
-  } else {
-    $filterAlert.html(null);
-  }
 }
 
 function createDataTable() {
-  //Create a sorter that uses case-insensitive html content
-  jQuery.extend(jQuery.fn.dataTableExt.oSort, {
-    "link-content-pre": function (a) {
-      return $(a).html().trim().toLowerCase();
-    },
+    //Create a sorter that uses case-insensitive html content
+    jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+        "link-content-pre": function (a) {
+            return $(a).html().trim().toLowerCase();
+        },
 
-    "link-content-asc": function (a, b) {
-      return a < b ? -1 : a > b ? 1 : 0;
-    },
+        "link-content-asc": function (a, b) {
+            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+        },
 
-    "link-content-desc": function (a, b) {
-      return a < b ? 1 : a > b ? -1 : 0;
-    },
-  });
+        "link-content-desc": function (a, b) {
+            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+        }
+    });
 
-  MyApp.oTable = $("#spreadsheet").dataTable({
-    aoColumnDefs: [{ sType: "link-content", aTargets: [0] }],
-    iDisplayLength: 20,
-    bLengthChange: false,
-    aaData: MyApp.spreadsheetData,
-    aoColumns: MyApp.headerData,
-  });
+    MyApp.oTable = $("#spreadsheet").dataTable({
+        "aoColumnDefs": [
+            { "sType": "link-content", "aTargets": [ 0 ] },
+            { "bVisible": false, "aTargets": [ -1 ] } //hide the keywords column for now (the last column, hence -1)
+        ],
+        "iDisplayLength": 20,
+        "bLengthChange": false,
+        "aaData": MyApp.spreadsheetData,
+        "aoColumns": MyApp.headerData
+    });
 }
